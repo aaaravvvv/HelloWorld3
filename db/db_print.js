f4d49a0627b830532a5b6edb3d0a10ba721827d6
aardud25 <aardud25@bergen.org>
2023-05-16 22:24:20 -0400

    Co-authored-by: mudasir  <mud-ali@users.noreply.github.com>

--- db/db_print.js
+++ db/db_print.js
@@ -1,4 +1,52 @@
 const db = require("./db_connection");
 
-const Users = "SELECT * FROM users";
+const pos = "SELECT * FROM pos";
+const users  = "SELECT * FROM users";
+const words = "SELECT * FROM words";
+const words_user_xref = "SELECT * FROM words_user_xref";
+
+
+db.execute(pos, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'pos' contents:")
+        console.log(results);
+    }
+);
+
+db.execute(users, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'users' contents:")
+        console.log(results);
+    }
+);
+
+db.execute(words, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'words' contents:")
+        console.log(results);
+    }
+);
+
+db.execute(words_user_xref, 
+    (error, results) => {
+        if (error) 
+            throw error;
+
+        console.log("Table 'words_user_xref' contents:")
+        console.log(results);
+    }
+);
+
+db.end();
+
+
 
